ee8d8ecaec5e6c6231d4b89a34ff59a1d66779d6
Amedee d'Aboville <amedee.daboville@gmail.com>
2019-05-19 17:01:14 -0400

    Add extra header types. They should all be there now. Unfortunately the format is very whitespace-specific/fragile, and hard to debug (should use nom-trace or similar). Going to add Unordered-ness to see if that helps.

--- src/lib.rs
+++ src/lib.rs
@@ -11,16 +11,21 @@ use std::result::Result;
 use strum::IntoEnumIterator;
 
 #[derive(Debug, PartialEq, Eq, Clone)]
-struct Coord(i32, N32, N32);
+struct Coord(i64, N32, N32);
 
 #[derive(Debug, PartialEq, Eq, Clone)]
-struct TSPLProblem {
+struct TSPLProblem<'a> {
     dimension: i64,
     coords: Vec<Coord>,
-    name: String,
-    comment: String,
+    name: &'a str,
+    comment: &'a str,
     problem_type: ProblemType,
+    capacity: Option<i64>,
     edge_weight_type: EdgeWeightType,
+    edge_weight_format: Option<EdgeWeightFormat>,
+    edge_data_format: Option<EdgeDataFormat>,
+    node_coord_type: NodeCoordType,
+    display_data_type: DisplayDataType,
 }
 
 // enum ProblemMeta {
@@ -55,6 +60,44 @@ enum EdgeWeightType {
     XRAY1,
     XRAY2,
     SPECIAL,
+}
+
+#[allow(non_camel_case_types)]
+#[derive(Debug, PartialEq, Eq, Clone, Display, EnumString, EnumIter)]
+enum EdgeWeightFormat {
+    FUNCTION,
+    FULL_MATRIX,
+    UPPER_ROW,
+    LOWER_ROW,
+    UPPER_DIAG_ROW,
+    LOWER_DIAG_ROW,
+    UPPER_COL,
+    LOWER_COL,
+    UPPER_DIAG_COL,
+    LOWER_DIAG_COL,
+}
+
+#[allow(non_camel_case_types)]
+#[derive(Debug, PartialEq, Eq, Clone, Display, EnumString, EnumIter)]
+enum EdgeDataFormat {
+    EDGE_LIST,
+    ADJ_LIST,
+}
+
+#[allow(non_camel_case_types)]
+#[derive(Debug, PartialEq, Eq, Clone, Display, EnumString, EnumIter)]
+enum NodeCoordType {
+    TWOD_COORDS,
+    THREED_COORDS,
+    NO_COORDS,
+}
+
+#[allow(non_camel_case_types)]
+#[derive(Debug, PartialEq, Eq, Clone, Display, EnumString, EnumIter)]
+enum DisplayDataType {
+    COORDS_DISPLAY,
+    TWOD_DISPLAY,
+    NO_DISPLAY,
 }
 
 named_args!(kv<'a>(key: &'a str)<&'a str, &'a str>,
@@ -136,6 +179,51 @@ fn test_edge_weight_type() {
     }
 }
 
+named!(get_edge_weight_format<&str, EdgeWeightFormat>,
+    map_res!(call!(kv, "EDGE_WEIGHT_FORMAT"), str::parse)
+);
+
+#[test]
+fn test_edge_weight_format() {
+    for ewf in EdgeWeightFormat::iter() {
+        test_kv(get_edge_weight_format, "EDGE_WEIGHT_FORMAT", ewf);
+    }
+}
+
+named!(get_edge_data_format<&str, EdgeDataFormat>,
+    map_res!(call!(kv, "EDGE_DATA_FORMAT"), str::parse)
+);
+
+#[test]
+fn test_edge_data_format() {
+    for edf in EdgeDataFormat::iter() {
+        test_kv(get_edge_data_format, "EDGE_DATA_FORMAT", edf);
+    }
+}
+
+named!(get_node_coord_type<&str, NodeCoordType>,
+    map_res!(call!(kv, "NODE_COORD_TYPE"), str::parse)
+);
+
+#[test]
+fn test_node_coord_type() {
+    for nct in NodeCoordType::iter() {
+        test_kv(get_node_coord_type, "NODE_COORD_TYPE", nct);
+    }
+}
+
+named!(get_display_data_type<&str, DisplayDataType>,
+    map_res!(call!(kv, "DISPLAY_DATA_TYPE"), str::parse)
+);
+
+#[test]
+fn test_display_data_type() {
+    for ddt in DisplayDataType::iter() {
+        test_kv(get_display_data_type, "DISPLAY_DATA_TYPE", ddt);
+    }
+}
+
+// node_coords: opt!(get_node_coord_section) >>
 named!(parse_problem<&str, TSPLProblem>,
     do_parse!(
         name: opt!(get_name) >>
@@ -143,13 +231,23 @@ named!(parse_problem<&str, TSPLProblem>,
         comment: opt!(get_comment) >>
         dimension: opt!(get_dimension) >>
         ewt: opt!(get_edge_weight_type) >>
+        capacity: opt!(get_capacity) >>
+        ewf: opt!(get_edge_weight_format) >>
+        edf: opt!(get_edge_data_format) >>
+        ddt: opt!(get_display_data_type) >>
+        nct: opt!(get_node_coord_type) >>
         (TSPLProblem {
-            name:name.unwrap_or("").to_string(),
+            name:name.unwrap_or(""),
             problem_type:ptype.unwrap(),
-            comment: comment.unwrap_or("").to_string(),
+            comment: comment.unwrap_or(""),
             dimension:dimension.unwrap(),
+            capacity: capacity,
             edge_weight_type:ewt.unwrap_or(EdgeWeightType::EUC_2D),
             coords: Vec::new(),
+            edge_data_format: edf,
+            edge_weight_format: ewf,
+            node_coord_type: nct.unwrap_or(NodeCoordType::NO_COORDS),
+            display_data_type: ddt.unwrap_or(DisplayDataType::NO_DISPLAY)
         })
     )
 );
@@ -171,7 +269,7 @@ fn test_2d_coords() {
     let input = " 1 1.0 3.0\n";
     assert_eq!(get_2d_coord(input), Ok(("", Coord(1, n32(1.0), n32(3.0)))));
 }
-named!(node_coord_section<&str, Vec<Coord> >,
+named!(get_node_coord_section<&str, Vec<Coord> >,
     do_parse!(
         tag_s!("NODE_COORD_SECTION") >>
         line_ending >>
@@ -194,7 +292,7 @@ EOF
         Coord(2, n32(25.0), n32(185.0)),
         Coord(3, n32(345.0), n32(750.0)),
     ];
-    assert_eq!(node_coord_section(ncs), Ok(("", out)))
+    assert_eq!(get_node_coord_section(ncs), Ok(("", out)))
 }
 #[test]
 fn test_parse_problem() {
@@ -205,12 +303,39 @@ DIMENSION: 52
 EDGE_WEIGHT_TYPE: EUC_2D
 ";
     let parsed = TSPLProblem {
-        name: "berlin52".to_string(),
+        name: "berlin52",
         problem_type: ProblemType::TSP,
-        comment: "52 locations in Berlin (Groetschel)".to_string(),
+        comment: "52 locations in Berlin (Groetschel)",
         dimension: 52,
         edge_weight_type: EdgeWeightType::EUC_2D,
         coords: Vec::new(),
+        capacity: None,
+        display_data_type: DisplayDataType::NO_DISPLAY,
+        edge_data_format: None,
+        edge_weight_format: None,
+        node_coord_type: NodeCoordType::NO_COORDS,
+    };
+    assert_eq!(parse_problem(header), Ok(("", parsed)))
+}
+#[test]
+fn test_parse_problem_works_with_missing_data() {
+    //atm name, comment, and ege weight type are optional.
+    let header = "TYPE: TSP
+DIMENSION: 52
+EDGE_WEIGHT_TYPE: EUC_2D
+";
+    let parsed = TSPLProblem {
+        name: "",
+        problem_type: ProblemType::TSP,
+        comment: "",
+        dimension: 52,
+        edge_weight_type: EdgeWeightType::EUC_2D,
+        coords: Vec::new(),
+        capacity: None,
+        display_data_type: DisplayDataType::NO_DISPLAY,
+        edge_data_format: None,
+        edge_weight_format: None,
+        node_coord_type: NodeCoordType::NO_COORDS,
     };
     assert_eq!(parse_problem(header), Ok(("", parsed)))
 }
