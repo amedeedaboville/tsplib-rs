c852d7c44b4ced5f0c3758fb4f3db47735dc4046
Amedee d'Aboville <amedee.daboville@gmail.com>
2019-05-22 00:09:50 -0400

    Remove individual get_ parsers, in favor of calling kv_parse directly in the main parser.

--- src/lib.rs
+++ src/lib.rs
@@ -107,175 +107,73 @@ named_args!(kv<'a>(key: &'a str)<&'a str, &'a str>,
     )
 );
 
-fn test_kv<'a, G: Display + Debug + PartialEq + Clone + 'a>(
-    kvfunc: fn(&str) -> Result<(&str, G), Err<(&str, nom::error::ErrorKind)>>,
+fn test_kv<'a, G: std::str::FromStr + Display + Debug + PartialEq + Clone + 'a>(
     key: &str,
     value: G,
 ) {
     let input = format!("{}: {}\n", key, value);
-    let output = kvfunc(&input);
+    let output: IResult<&str, G> = kv_parse(&input, key);
     assert_eq!(output, Ok(("", value)));
 }
 
-// fn kv_fn<'a>(input: &str, key: &'a str) -> IResult<&'a str, &'a str>,
-//    do_parse!(
-//         tag!(key) >>
-//         tag!(":") >>
-//         space0 >>
-//         value: not_line_ending >>
-//         line_ending >>
-//         (value)
-//     )
-// }
-
-// named_args!(kv_parse<'a>(key: &'a str)<&'a str, &'a T>,
-// map_res!(call!(kv, key), str::parse::<T>)
-// do_parse!(
-//     input,
-//     tag!(key)
-//         >> tag!(":")
-//         >> space0
-//         >> value: not_line_ending
-//         >> line_ending
-//         >> (value)
-// ),
-// fn many_reads(input:&[u8]) -> IResult<&[u8], Vec<Sequence>> {
 fn kv_parse<'a, T>(input: &'a str, key: &'a str) -> IResult<&'a str, T>
 where
     T: std::str::FromStr,
-    <T as std::str::FromStr>::Err: std::fmt::Debug,
 {
-    let kv_out = kv(input, key);
-    match kv_out {
-        Ok((i, v)) => {
-            let parse_res = str::parse::<T>(v);
-            match parse_res {
-                Ok(tv) => Ok((i, tv)),
-                Err(a) => Err(Err::Error((i, nom::error::ErrorKind::ParseTo)))
-                // Err(a) => Err(nom::Err::Error(nom::Context::Code(i, ErrorKind::Custom("Parse error"))))
-                // Err(a) => nom::Err(Err::Err(a)),
-            }
-        }
-        Err(a) => Err(a),
-    }
-    // kv_out.map(|(i, v): (&str, &str)| str::parse(v).map(|tv| (i, tv)).unwrap())
-    // kv_out.map(|(i, v): (&str, &str)| (i, str::parse(v)))
+    kv(input, key).and_then(|(i, v)| match str::parse::<T>(v) {
+        Ok(tv) => Ok((i, tv)),
+        Err(a) => Err(Err::Error((i, nom::error::ErrorKind::ParseTo))),
+    })
 }
-named!(get_name<&str,&str>,
-    call!(kv, "NAME")
-);
 #[test]
 fn test_name() {
-    assert_eq!(get_name("NAME: some_name45\n"), Ok(("", "some_name45")));
+    test_kv("NAME", "some_name".to_string());
 }
-named!(get_comment<&str,&str>,
-    call!(kv, "COMMENT")
-);
 #[test]
 fn test_comment() {
-    let comment = "My favorite TSP instance: Minimum tour around all of my friend's fridges";
-    assert_eq!(
-        get_comment(&format!("COMMENT: {}\n", comment)),
-        Ok(("", comment))
-    );
-}
-
-fn get_type(input: &str) -> IResult<&str, ProblemType> {
-    kv_parse::<ProblemType>(input, "TYPE")
-}
-
-#[test]
-fn test_type() {
-    for ptype in ProblemType::iter() {
-        test_kv(get_type, "TYPE", ptype);
-    }
-}
-
-fn get_dimension(input: &str) -> IResult<&str, i64> {
-    kv_parse(input, "DIMENSION")
-}
-#[test]
-fn test_dimension() {
-    test_kv(get_dimension, "DIMENSION", 8)
-}
-#[test]
-fn test_dimension_invalid() {
-    let output = get_dimension("DIMENSION: ABDC\n");
-    assert_eq!(
-        output,
-        Err((nom::Err::Error(("", nom::error::ErrorKind::ParseTo))))
-    )
-}
-
-fn get_capacity(input: &str) -> IResult<&str, i64> {
-    kv_parse(input, "CAPACITY")
-}
-
-#[test]
-fn test_capacity() {
-    test_kv(get_capacity, "CAPACITY", 8)
-}
-
-fn get_edge_weight_type(input: &str) -> IResult<&str, EdgeWeightType> {
-    kv_parse(input, "EDGE_WEIGHT_TYPE")
+    let comment =
+        "My favorite TSP instance: Minimum tour around all of my friend's fridges".to_string();
+    test_kv("COMMENT", comment);
 }
 
 #[test]
 fn test_edge_weight_type() {
     for ewt in EdgeWeightType::iter() {
-        test_kv(get_edge_weight_type, "EDGE_WEIGHT_TYPE", ewt);
+        test_kv("EDGE_WEIGHT_TYPE", ewt);
     }
 }
-
-fn get_edge_weight_format(input: &str) -> IResult<&str, EdgeWeightFormat> {
-    kv_parse(input, "EDGE_WEIGHT_FORMAT")
-}
-
 #[test]
 fn test_edge_weight_format() {
     for ewf in EdgeWeightFormat::iter() {
-        test_kv(get_edge_weight_format, "EDGE_WEIGHT_FORMAT", ewf);
+        test_kv("EDGE_WEIGHT_FORMAT", ewf);
     }
 }
-
-named!(get_edge_data_format<&str, EdgeDataFormat>,
-    map_res!(call!(kv, "EDGE_DATA_FORMAT"), str::parse)
-);
 
 #[test]
 fn test_edge_data_format() {
     for edf in EdgeDataFormat::iter() {
-        test_kv(get_edge_data_format, "EDGE_DATA_FORMAT", edf);
+        test_kv("EDGE_DATA_FORMAT", edf);
     }
 }
-
-named!(get_node_coord_type<&str, NodeCoordType>,
-    map_res!(call!(kv, "NODE_COORD_TYPE"), str::parse)
-);
 
 #[test]
 fn test_node_coord_type() {
     for nct in NodeCoordType::iter() {
-        test_kv(get_node_coord_type, "NODE_COORD_TYPE", nct);
+        test_kv("NODE_COORD_TYPE", nct);
     }
 }
-
-named!(get_display_data_type<&str, DisplayDataType>,
-    map_res!(call!(kv, "DISPLAY_DATA_TYPE"), str::parse)
-);
-
 #[test]
 fn test_display_data_type() {
     for ddt in DisplayDataType::iter() {
-        test_kv(get_display_data_type, "DISPLAY_DATA_TYPE", ddt);
+        test_kv("DISPLAY_DATA_TYPE", ddt);
     }
 }
-// /*
+
 fn build_problem(
     (name, problem_type, comment, dimension, ewt, capacity, ewf, edf, ddt, nct): (
-        Option<&str>,
+        Option<String>,
         Option<ProblemType>,
-        Option<&str>,
+        Option<String>,
         Option<i64>,
         Option<EdgeWeightType>,
         Option<i64>,
@@ -286,9 +184,9 @@ fn build_problem(
     ),
 ) -> TSPLProblem {
     TSPLProblem {
-        name: name.unwrap_or("").to_string(),
+        name: name.unwrap_or("".to_string()),
         problem_type: problem_type.unwrap(),
-        comment: comment.unwrap_or("").to_string(),
+        comment: comment.unwrap_or("".to_string()),
         dimension: dimension.unwrap(),
         capacity: capacity,
         edge_weight_type: ewt.unwrap_or(EdgeWeightType::EUC_2D),
@@ -302,77 +200,59 @@ fn build_problem(
 fn parse_problem_perm(input: &str) -> IResult<&str, TSPLProblem> {
     map!(
         input,
-        dbg_dmp!(permutation!(
-            get_name?,
-            get_type?,
-            get_comment?,
-            get_dimension?,
-            get_edge_weight_type?,
-            get_capacity?,
-            get_edge_weight_format?,
-            get_edge_data_format?,
-            get_display_data_type?,
-            opt!(get_node_coord_type)
-        )),
+        permutation!(
+            call!(kv_parse, "NAME")?,
+            call!(kv_parse, "TYPE")?,
+            call!(kv_parse, "COMMENT")?,
+            call!(kv_parse, "DIMENSION")?,
+            call!(kv_parse, "EDGE_WEIGHT_TYPE")?,
+            call!(kv_parse, "CAPACITY")?,
+            call!(kv_parse, "EDGE_WEIGHT_FORMAT")?,
+            call!(kv_parse, "EDGE_DATA_FORMAT")?,
+            call!(kv_parse, "DISPLAY_DATA_TYPE")?,
+            opt!(call!(kv_parse, "NODE_COORD_TYPE"))
+        ),
         build_problem
     )
 }
 
-named!(parse_problem<&str, TSPLProblem>,
+fn get_section<'a, T>(
+    input: &'a str,
+    section_title: &'a str,
+    section_parser: fn(&str) -> IResult<&str, T>,
+) -> IResult<&'a str, Vec<T>> {
     do_parse!(
-        name: opt!(get_name) >>
-        ptype: opt!(get_type) >>
-        comment: opt!(get_comment) >>
-        dimension: opt!(get_dimension) >>
-        ewt: opt!(get_edge_weight_type) >>
-        capacity: opt!(get_capacity) >>
-        ewf: opt!(get_edge_weight_format) >>
-        edf: opt!(get_edge_data_format) >>
-        ddt: opt!(get_display_data_type) >>
-        nct: opt!(get_node_coord_type) >>
-        (TSPLProblem {
-            name:name.unwrap_or("").to_string(),
-            problem_type:ptype.unwrap(),
-            comment: comment.unwrap_or("").to_string(),
-            dimension:dimension.unwrap(),
-            capacity: capacity,
-            edge_weight_type:ewt.unwrap_or(EdgeWeightType::EUC_2D),
-            coords: Vec::new(),
-            edge_data_format: edf,
-            edge_weight_format: ewf,
-            node_coord_type: nct.unwrap_or(NodeCoordType::NO_COORDS),
-            display_data_type: ddt.unwrap_or(DisplayDataType::NO_DISPLAY)
-        })
+        input,
+        tag!(section_title)
+            >> line_ending
+            >> payload: many1!(section_parser)
+            >> opt!(complete!(tag!("EOF\n")))
+            >> (payload)
     )
-);
-
-named!(get_2d_coord<&str, Coord>,
+}
+fn get_2d_coord(input: &str) -> IResult<&str, Coord> {
     do_parse!(
-        opt!(multispace1) >>
-         i: digit1 >>
-            space1 >>
-         x: float >>
-            space1 >>
-         y: float >>
-         line_ending >>
-         (Coord( i.parse().unwrap(), n32(x), n32(y))))
-);
+        input,
+        opt!(multispace1)
+            >> i: digit1
+            >> space1
+            >> x: float
+            >> space1
+            >> y: float
+            >> line_ending
+            >> (Coord(i.parse().unwrap(), n32(x), n32(y)))
+    )
+}
 
 #[test]
 fn test_2d_coords() {
     let input = " 1 1.0 3.0\n";
     assert_eq!(get_2d_coord(input), Ok(("", Coord(1, n32(1.0), n32(3.0)))));
 }
-named!(get_node_coord_section<&str, Vec<Coord> >,
-    do_parse!(
-        tag!("NODE_COORD_SECTION") >>
-        line_ending >>
-        coords: many1!(get_2d_coord) >>
-        opt!(complete!(tag!("EOF\n"))) >>
-        (coords)
-    )
-);
 
+fn get_node_coord_section(input: &str) -> IResult<&str, Vec<Coord>> {
+    get_section(input, "NODE_COORD_SECTION", get_2d_coord)
+}
 #[test]
 fn test_node_coord_section() {
     let ncs = "NODE_COORD_SECTION
@@ -387,6 +267,9 @@ EOF
         Coord(3, n32(345.0), n32(750.0)),
     ];
     assert_eq!(get_node_coord_section(ncs), Ok(("", out)))
+}
+fn get_edge_weight_section(input: &str) -> IResult<&str, Vec<Coord>> {
+    get_section(input, "EDGE_WEIGHT_SECTION", get_2d_coord)
 }
 #[test]
 fn test_parse_problem() {
